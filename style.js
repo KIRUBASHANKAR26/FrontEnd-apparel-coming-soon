582cf2ed41988484fec009a15d059334b3fcd3fc
Kirubashankar K <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2021-04-04 20:46:51 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,30 @@
+
+function check()
+{
+  var emailInput=document.getElementById("email-input");
+  var btn=document.getElementById("text-error");
+  var errorIcon=document.getElementById("error-img");
+  if(!emailInput.checkValidity())
+  {
+    errorIcon.classList.add("show");
+    emailInput.style.border="3px solid hsl(0, 93%, 68%)";
+    var errorText=document.createElement('p');
+    errorText.className='error-text';
+    var textNode=document.createTextNode('Please provide a valid mail');
+    errorText.appendChild(textNode);
+    btn.appendChild(errorText);
+    emailInput.value="";
+   
+  }else
+  {
+      emailInput.style.border = "1px solid hsl(0, 36%, 70%)";
+      var errorText=document.createElement('p');
+      errorText.className='error-text';
+      var textNode=document.createTextNode('Thanks,We\'re coming soon!');
+      errorText.appendChild(textNode);
+      btn.appendChild(errorText);
+      emailInput.value="";
+  }
+}
+
+
